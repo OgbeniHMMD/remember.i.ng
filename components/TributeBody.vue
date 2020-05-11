ddb5aa5153d5fc871ceb853b26fa425149a4e2ef
Olajide A. Hammed <Olajide.A.Hammed@gmail.com>
2020-05-11 16:37:05 +0100

    Mock up begins

---
+++ components/TributeBody.vue
@@ -0,0 +1,34 @@
+<template >
+  <div
+    class="d-flex justify-content-center align-items-center text-center min-vh-100 border-dark border-bottom m-0 my-auto"
+  >
+    <div>
+      <logo />
+      <h1 class="display-1 text-secondary">remember.i.ng</h1>
+      <h2 class="h1 text-dark">My Collection Of Tribute Pages</h2>
+      <div class="mt-4">
+        <a
+          href="https://nuxtjs.org/"
+          target="_blank"
+          class="btn btn-lg btn-outline-primary mr-4"
+        >Documentation</a>
+        <a
+          href="https://github.com/nuxt/nuxt.js"
+          target="_blank"
+          class="btn btn-lg btn-outline-dark mr-4"
+        >GitHub</a>
+      </div>
+    </div>
+  </div>
+</template>
+
+
+<script>
+import Logo from "~/components/Logo.vue";
+
+export default {
+  components: {
+    Logo
+  }
+};
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,33 +1,21 @@
 <template>
-  <div
-    class="d-flex justify-content-center align-items-center text-center min-vh-100 border-dark border-bottom m-0 my-auto"
-  >
-    <div>
-      <logo />
-      <h1 class="display-1 text-secondary">remember.i.ng</h1>
-      <h2 class="h1 text-dark">My Collection Of Tribute Pages</h2>
-      <div class="mt-4">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="btn btn-lg btn-outline-primary mr-4"
-        >Documentation</a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="btn btn-lg btn-outline-dark mr-4"
-        >GitHub</a>
-      </div>
-    </div>
+  <div>
+    <tribute-header />
+    <tribute-body />
+    <tribute-footer />
   </div>
 </template>
 
 <script>
-import Logo from "~/components/Logo.vue";
+import TributeHeader from "~/components/TributeHeader.vue";
+import TributeBody from "~/components/TributeBody.vue";
+import TributeFooter from "~/components/TributeFooter.vue";
 
 export default {
   components: {
-    Logo
+    TributeHeader,
+    TributeBody,
+    TributeFooter
   }
 };
 </script>
